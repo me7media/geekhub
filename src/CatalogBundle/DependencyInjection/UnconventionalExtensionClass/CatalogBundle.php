a132cbb8f4a03373d35bf23cf14a637f189bf386
me_media <vgoogle@i.ua>
2020-01-11 01:50:42 +0200

    extention

--- src/CatalogBundle/DependencyInjection/UnconventionalExtensionClass/CatalogBundle.php
+++ src/CatalogBundle/DependencyInjection/UnconventionalExtensionClass/CatalogBundle.php
@@ -5,14 +5,19 @@ namespace App\CatalogBundle\DependencyInjection\UnconventionalExtensionClass;
 
 use App\CatalogBundle\DependencyInjection\CatalogBundleExtension;
 use App\CatalogBundle\Twig\Catalog_Twig_Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class CatalogBundle extends Bundle
 {
 
+    public function build(ContainerBuilder $container)
+    {
+        $container->registerExtension(new CatalogBundleExtension());
+    }
+
     public function boot()
     {
-        parent::boot();
         $loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/../Resources/views');
         $twig = new \Twig\Environment($loader);
         $twig->addExtension(new Catalog_Twig_Extension());
@@ -20,7 +25,6 @@ class CatalogBundle extends Bundle
 
     public function getContainerExtension()
     {
-            parent::getContainerExtension();
         return new CatalogBundleExtension();
     }
 }
